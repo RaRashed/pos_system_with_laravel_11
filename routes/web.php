a65d353f213d62eed355599c019ccdb5dde1e5a3
RaRAshed <rnrashedrn@gmail.com>
2025-01-22 10:40:03 -0800

    admin auth set

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Admin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
 
 Route::get('/', function () {
-    if (Auth::check()) {
+    // $admin = new Admin();
+    // $admin->name = "rashed";
+    // $admin->email = "admin@admin.com";
+    // $admin->password = Hash::make('12345678');
+    // $admin->save();
+    if (Auth::guard('admin')->check()) {
         return view('dashboard');
     }
     return view('welcome');
@@ -13,12 +20,16 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(middleware: ['auth:admin', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+
+Route::prefix('admin')->middleware(['auth:admin'])->name('admin.')->group(function () {
+    // Admin profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::patch('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile/destroy', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Add other admin routes as needed
 });
 
 require __DIR__ . '/auth.php';
